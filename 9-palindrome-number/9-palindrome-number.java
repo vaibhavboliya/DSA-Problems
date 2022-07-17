158325d67577ebe51f3382eb5b2ee74051fc6c8b
Vaibhav Boliya <53927146+vaibhavboliya@users.noreply.github.com>
2022-07-17 21:57:15 +0530

    Time: 14 ms (60.41%), Space: 45 MB (40.35%)

---
+++ 9-palindrome-number/9-palindrome-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isPalindrome(int x) {  
+        if (x<0) return false;
+        int rev = 0;
+        int y = x;
+        while(y!=0){
+            rev= rev*10 + y % 10;
+            y = y/10;
+        }
+        return x==rev; 
+    }
+}
