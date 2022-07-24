9c6abd67076a64057465e736b3bbbb506316bd69
Vaibhav Boliya <53927146+vaibhavboliya@users.noreply.github.com>
2022-07-24 17:40:37 +0530

    Time: 4 ms (56.20%), Space: 43.6 MB (61.78%)

---
+++ 1436-destination-city/1436-destination-city.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> cities = new HashSet<>();
+        for(List<String> path : paths){
+            cities.add(path.get(0));
+        }
+        for(List<String> path:paths){
+            String des= path.get(1);
+            if(!cities.contains(des))
+                return des;
+            
+        }
+        return "";
+        
+    }
+}
