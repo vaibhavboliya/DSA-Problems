41ec95605adca71b4748ea7e121b21e1beb90f60
Vaibhav Boliya <53927146+vaibhavboliya@users.noreply.github.com>
2022-07-24 21:15:29 +0530

    Time: 1 ms (79.19%), Space: 41.5 MB (79.12%)

---
+++ 2103-rings-and-rods/2103-rings-and-rods.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int countPoints(String rings) {
+        int r[] = new int[10];
+        int g[] = new int[10];
+        int b[] = new int[10];
+        
+        for(int i =0;i<rings.length();i+=2){
+            int a = rings.charAt(i+1) -'0';
+            if(rings.charAt(i)=='R')
+                r[a] ++;
+            if(rings.charAt(i)=='G')
+                g[a] ++;
+            if(rings.charAt(i)=='B')
+                b[a] ++;
+        }
+        int count = 0;
+        for(int i=0;i<10;i++)
+            if(r[i]>0 && g[i]>0 && b[i]>0)
+                count++;
+        return count;
+        
+    }
+}
