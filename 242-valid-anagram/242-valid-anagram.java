5839652b31575b2390381ed087da8773c70d5a04
Vaibhav Boliya <53927146+vaibhavboliya@users.noreply.github.com>
2022-07-31 20:34:20 +0530

    Time: 3 ms (94.86%), Space: 43.4 MB (76.14%)

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -1,10 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char ch1[] = s.toCharArray();
-        char ch2[] = t.toCharArray();
-        Arrays.sort(ch1);
-        Arrays.sort(ch2);
-        return Arrays.equals(ch1,ch2);
+        int ch[] = new int[26];
+        for(int i=0;i<s.length();i++) 
+            ch[s.charAt(i) -'a']++;
+        for(int i=0;i<t.length();i++) 
+            ch[t.charAt(i) - 'a']--;
+        for(int i:ch) if(i !=0) return false;
+        return true;
         
     }
 }
